ab17b2a33ea13a0a4f99b42cc298076cb25041a1
valejandrov <alejandro_vasile@hotmail.com>
2019-08-22 13:16:46 -0300

    Se agregan los comentarios sobre el challenge

---
+++ HackerrankJavaEasy/src/EasyChallenges/DiagonalDifference.java
@@ -0,0 +1,26 @@
+/*
+    Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+
+For example, the square matrix  is shown below:
+
+1 2 3
+4 5 6
+9 8 9  
+The left-to-right diagonal = . The right to left diagonal = . Their absolute difference is .
+
+Function description
+Complete the  function in the editor below. It must return an integer representing the absolute diagonal difference.
+
+diagonalDifference takes the following parameter:
+
+arr: an array of integers .
+
+ */
+package EasyChallenges;
+
+/**
+    @author Alejandro
+ */
+public class DiagonalDifference {
+    
+}
